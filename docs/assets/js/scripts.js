cf8eac5b595888beb0913d4eb8b2ca3b47cef82f
Thaddaeus Dahlberg <16785863+tcdahlberg@users.noreply.github.com>
2024-11-13 14:26:16 -0600

    force rebuild of site

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-  setModifiedDate();
+    setModifiedDate();
 });
 
 function setModifiedDate() {
-  fetch("https://api.github.com/repos/" + ownerName + "/" + repoName + "/commits?path=" + pagePath)
-    .then((response) => {
-      return response. json();
-    })
-    .then ((commits) => {
-      console.log(JSON.stringify(commits[0]));
-      let modified = commits[0]['commit']['committer']['date'].slice(0, 10);
-      let author = commits[0]['commit']['author']['name'];
-      document.querySelectorAll(".last-modified").forEach(function (lastModifiedElement) {
-        lastModifiedElement.textContent = "Last Modified: " + modified + ' by ' + author;
-      });
-    });
+    fetch("https://api.github.com/repos/" + ownerName + "/" + repoName + "/commits?path=" + pagePath)
+        .then((response) => {
+            return response.json();
+        })
+        .then((commits) => {
+            console.log(JSON.stringify(commits[0]));
+            let modified = commits[0]['commit']['committer']['date'].slice(0, 10);
+            let author = commits[0]['commit']['author']['name'];
+            document.querySelectorAll(".last-modified").forEach(function (lastModifiedElement) {
+                lastModifiedElement.textContent = "Last Modified: " + modified + ' by ' + author;
+            });
+        });
 }
