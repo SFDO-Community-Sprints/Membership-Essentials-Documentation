ff6401b4b3968e40fd4fe69a5e906463373db895
Thaddaeus Dahlberg <16785863+tcdahlberg@users.noreply.github.com>
2024-11-13 12:39:15 -0600

    fixed script syntax

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -10,8 +10,8 @@ function setModifiedDate() {
     .then ((commits) => {
       console.log(JSON.stringify(commits[0]));
       let modified = commits[0]['commit']['committer']['date'].slice(0, 10);
-      let author = commits [0]['commit ']['author']['name'];
-      document-querySelectorAll(".last-modified").forEach(function (lastModifiedElement) {
+      let author = commits[0]['commit']['author']['name'];
+      document.querySelectorAll(".last-modified").forEach(function (lastModifiedElement) {
         lastModifiedElement.textContent = "Last Modified: " + modified + ' by ' + author;
       });
     });
