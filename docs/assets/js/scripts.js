6141ed40912e0ff9cfd683e53ee5dfd9040e8dd6
Sandy Whitten <37025590+SWhitten@users.noreply.github.com>
2024-11-13 11:32:30 -0500

    Create scripts.js
    Created assesset/js/scripts.js under docs folder

---
+++ docs/assets/js/scripts.js
@@ -0,0 +1,14 @@
+function setModifiedDate() {
+  fetch("https://api.jithub.com/repos/" + ownerName + "/" + repoName + "/commits?path=" + pagePath)
+    .then((response) => {
+      return response. json();
+    })
+    .then ((commits) => {
+      console.log(JSON.stringify(commits[0]));
+      let modified = commits[0]['commit']['committer']['date'].slice(0, 10);
+      let author = commits [0]['commit ']['author']['name'];
+      document-querySelectorAll(".last-modified").forEach(function (lastModifiedElement) {
+        lastModifiedElement.textContent = "Last Modified: " + modified + ' by ' + author;
+      });
+    });
+}
